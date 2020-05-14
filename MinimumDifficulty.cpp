7a666282265d764e2f4f4eaa9ad5305d86f1411c
Nikhil Subhash Jugale <52400252+nikhiljugale007@users.noreply.github.com>
2020-05-14 15:48:30 +0530

    Greg's Workout (255 A)

--- MinimumDifficulty.cpp
+++ MinimumDifficulty.cpp
@@ -1,25 +1,64 @@
+//********nikhiljugale007**********//
 
 #include<bits/stdc++.h>
-#define maxx 11111
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include<algorithm>
+#include<set>
+#include<iterator>
+
 using namespace std;
-int a[maxx];
+
+#define fast        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll          long long 
+#define v           vector<ll int>
+#define it          iterator it
+#define pb          push_back
+#define f(a,b)      for(long long int i=a;i<b;i++)
+#define fv(v)       for(auto i=v.begin() ; i!=v.end();i++)
+#define min         min_element
+#define max         max_element
+#define hell        1000000007
+
+
 int main(){
-    int i,j,n,ans=9999;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        cin>>a[i];
+
+    fast;
+    ll int n;
+    cin>>n;
+
+    int arr[n+1];
+
+    f(1,n+1){
+        cin>>arr[i];
     }
-    for(i=2;i<n;i++){
-        int t=0;
-        for(j=2;j<=n;j++){
-            if(j==i) continue;
-            if(j==i+1){
-                t=max(t,a[j]-a[j-2]);
-                continue;
-            }
-            t=max(t,a[j]-a[j-1]);
+    
+    int sum1=0 , sum2=0 , sum3=0 ;
+
+    f(1,n+1){
+        switch(i%3){
+            case 1:
+                sum1+=arr[i];
+                break;
+            case 2:
+                sum2+=arr[i];
+                break;
+            case 0:
+                sum3+=arr[i];
+                break;
         }
-        ans=min(ans,t);
     }
-    cout<<ans;
+    
+    if(sum1>sum2 && sum1>sum3){
+        cout<<"chest"<<endl;
+    }
+    else if(sum2>sum3 && sum2>sum1){
+        cout<<"biceps"<<endl;
+    }
+    else if(sum3>sum1 && sum3>sum2){
+        cout<<"back"<<endl;    
+    }
+
+    return 0;
 }
